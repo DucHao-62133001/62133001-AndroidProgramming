6166eaf0b5ee3d908f4400c254dc4e34d3fc89e2
DucHao-62133001 <hao.nd.62cnot@ntu.edu.vn>
2026-04-20 07:39:14 +0700

    Push-LAN THU 1

---
+++ thigk2nguyenduchao/app/src/main/java/com/hao/thigk2nguyenduchao/ChucNang1Activity.java
@@ -0,0 +1,4 @@
+package com.hao.thigk2nguyenduchao;
+
+public class ChucNang1Activity {
+}
--- thigk2nguyenduchao/app/src/main/java/com/hao/thigk2nguyenduchao/MainActivity.java
+++ thigk2nguyenduchao/app/src/main/java/com/hao/thigk2nguyenduchao/MainActivity.java
@@ -1,24 +1,34 @@
 package com.hao.thigk2nguyenduchao;
 
+import android.content.Intent;
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
+import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btn1, btn2, btn3, btn4; //Khai bao
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        btn1 = findViewById(R.id.btn1);
+        btn2 = findViewById(R.id.btn2);
+        btn3 = findViewById(R.id.btn3);
+        btn4 = findViewById(R.id.btn4);
+
+        btn1.setOnClickListener(v ->
+                startActivity(new Intent(MainActivity.this, ChucNang1Activity.class)));
+
+        btn2.setOnClickListener(v ->
+                startActivity(new Intent(MainActivity.this, ChucNang2Activity.class)));
+
+        btn3.setOnClickListener(v ->
+                startActivity(new Intent(MainActivity.this, ChucNang3Activity.class)));
+
+        btn4.setOnClickListener(v ->
+                startActivity(new Intent(MainActivity.this, ChucNang4Activity.class)));
     }
 }
